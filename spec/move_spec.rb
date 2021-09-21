b31d99e8532a877ec82793239fc1e2d62643fd1f
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-20 22:02:26 -0600

    Begin writing Board#move spec file
    - Start segementing contexts based on turn
    - Write tests for pawns and begin writing tests for
      knights on the first move

---
+++ spec/move_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../lib/board'
+
+# Require all the chess pieces
+Dir.children('./lib/chess_pieces').each { |piece_file| require_relative "../lib/chess_pieces/#{piece_file}" }
+
+RSpec.describe 'Board#move' do
+  let(:board) { Board.new }
+  let(:game_board) { board.instance_variable_get(:@game_board) }
+  describe 'first turn' do
+
+    context 'when a white pawn moves' do
+      it 'lets pawn move forward one' do
+        8.times do |col|
+          expect(proc { board.move([1, col], [2, col]) }).to change { game_board[1][col] }.to('  ')
+        end
+      end
+
+      it 'lets pawn move forward two' do
+        8.times do |col|
+          expect(proc { board.move([1, col], [3, col]) }).to change { game_board[1][col] }.to('  ')
+        end
+      end
+
+      it 'flags pawn as having moved afterward' do
+        8.times do |col|
+          board.move([1, col], [3, col])
+          expect(game_board[3][col].has_moved).to be true
+        end
+      end
+    end
+
+    context 'when a white knight moves' do
+      it 'lets knight move to valid space' do
+        expect(proc { board.move([0, 1], [2, 0]) }).to change { game_board[0][1] }.to('  ')
+        expect(proc { board.move([0, 1], [2, 2]) }).to change { game_board[0][1] }.to('  ')
+        expect(proc { board.move([0, 6], [2, 5]) }).to change { game_board[0][6] }.to('  ')
+        expect(proc { board.move([0, 6], [2, 7]) }).to change { game_board[0][6] }.to('  ')
+      end
+    end
+
+    context 'when black pawn moves' do
+      it 'raises InvalidMoveError' do
+        expect(proc { board.move([6, 0], [5, 0]) }).to raise_error(InvalidMoveError)
+      end
+    end
+  end
+end
