5d47f49ba686ad9357c8a87532ca2ea4b5093032
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-26 00:46:48 -0600

    Create move tree template for the Rook
    - Remove unnecessary @direction attribute
    - Create a helper function for creating a hierarchy of moves
      where each move is the child of the next closest legal move
      of the piece

--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -1,14 +1,44 @@
 # frozen_string_literal: true
 
+require 'colorize'
 require_relative 'chess_piece'
 
 class Rook < ChessPiece
-  attr_reader :direction
-
   ##
   # Initializes a new pawn
   def initialize(color, position)
-    @direction = color == 'white' ? 1 : -1
+    @move_tree_template = build_rook_move_tree
     super(color == 'white' ? '♜'.white : '♖', color, position, 5)
+  end
+
+  protected
+
+  ##
+  # Add children to move tree nodes such that each move is a child node of
+  # the move that precedes it.
+  def build_directional_tree_nodes(direction = [1, 0])
+    vertical_movement, horizontal_movement = direction
+    closest_move = MoveTreeNode.new(direction)
+    (2..8).each do |spaces|
+      current_child = closest_move
+      (spaces - 2).times { current_child = current_child.children[0] }
+      current_child.add_child([spaces * vertical_movement, spaces * horizontal_movement])
+    end
+
+    closest_move
+  end
+
+  ##
+  # Buildsthe rook's template build tree
+  def build_rook_move_tree
+    move_tree = MoveTree.new([0, 0])
+
+    # Build in each of the four directions the rook can go.
+    move_tree.root.add_child(build_directional_tree_nodes([1, 0]))
+    move_tree.root.add_child(build_directional_tree_nodes([-1, 0]))
+    move_tree.root.add_child(build_directional_tree_nodes([0, 1]))
+    move_tree.root.add_child(build_directional_tree_nodes([0, -1]))
+
+    move_tree
   end
 end
