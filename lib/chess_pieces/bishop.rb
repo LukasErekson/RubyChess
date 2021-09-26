e9aa631ab3bbd9f7617b3c0250e1ab55eb46652a
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-26 00:56:50 -0600

    Fix unnoticed merge conflicts in several pieces

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -3,10 +3,6 @@
 require_relative 'chess_piece'
 
 class Bishop < ChessPiece
-<<<<<<< HEAD
-=======
-
->>>>>>> f51bab81f3a52469847f51a7475e35268837134b
   ##
   # Initializes a new pawn
   def initialize(color, position)
