349513e867911ce87f147984e2880265bea67ffb
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-26 00:49:26 -0600

    Remove @direction attribute from most chess pieces
    - Remove @direction from most chess pieces as the pawn
      is the only piece that cannot move backward and all
      other moves should still be available using the MoveTree
      structure
    - Move #build_directional_tree_nodes to be ChessPiece
      because it will be helpful to for bishops and queens
      as well as rooks

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -3,12 +3,9 @@
 require_relative 'chess_piece'
 
 class Bishop < ChessPiece
-  attr_reader :direction
-
   ##
   # Initializes a new pawn
   def initialize(color, position)
-    @direction = color == 'white' ? 1 : -1
     super(color == 'white' ? '♝'.white : '♗', color, position, 3)
   end
 end
--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'colorize'
 require_relative 'chess_piece'
 
 class Rook < ChessPiece
@@ -12,21 +11,6 @@ class Rook < ChessPiece
   end
 
   protected
-
-  ##
-  # Add children to move tree nodes such that each move is a child node of
-  # the move that precedes it.
-  def build_directional_tree_nodes(direction = [1, 0])
-    vertical_movement, horizontal_movement = direction
-    closest_move = MoveTreeNode.new(direction)
-    (2..8).each do |spaces|
-      current_child = closest_move
-      (spaces - 2).times { current_child = current_child.children[0] }
-      current_child.add_child([spaces * vertical_movement, spaces * horizontal_movement])
-    end
-
-    closest_move
-  end
 
   ##
   # Buildsthe rook's template build tree
