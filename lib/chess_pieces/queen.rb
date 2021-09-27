38e8e4d4b1a905aec11939b5b92591e603b9d53d
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-27 15:36:30 -0600

    Implement Queen move tree

--- lib/chess_pieces/queen.rb
+++ lib/chess_pieces/queen.rb
@@ -8,6 +8,25 @@ class Queen < ChessPiece
   ##
   # Initializes a new pawn
   def initialize(color, position)
+    @move_tree_template = build_queen_move_tree
     super(color == 'white' ? '♛'.white : '♕', color, position, 9)
+  end
+
+  protected
+
+  ##
+  # Builds the Queen's move tree. The Queen can move in any direction as far as
+  # the board permits.
+  def build_queen_move_tree
+    move_tree_template = MoveTree.new([0, 0])
+    # Get directions
+    directions = [-1, 0, 1].repeated_permutation(2).to_a
+    directions.delete([0, 0])
+
+    directions.each do |direction|
+      move_tree_template.root.add_child(build_directional_tree_nodes(direction))
+    end
+
+    move_tree_template
   end
 end
