16a01b7dff61c3c339a5f8856aa2566fd72a366b
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-26 19:17:03 -0600

    Implment Bishop::@move_tree_template

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -8,6 +8,22 @@ class Bishop < ChessPiece
   ##
   # Initializes a new pawn
   def initialize(color, position)
+    @move_tree_template = build_bishop_move_tree
     super(color == 'white' ? '♝'.white : '♗', color, position, 3)
+  end
+
+  protected
+
+  ##
+  # Build a bishop's move tree with only diagonal moves.
+  def build_bishop_move_tree
+    move_tree_template = MoveTree.new([0, 0])
+
+    move_tree_template.root.add_child(build_directional_tree_nodes([1, 1]))
+    move_tree_template.root.add_child(build_directional_tree_nodes([-1, 1]))
+    move_tree_template.root.add_child(build_directional_tree_nodes([1, -1]))
+    move_tree_template.root.add_child(build_directional_tree_nodes([-1, -1]))
+
+    move_tree_template
   end
 end
