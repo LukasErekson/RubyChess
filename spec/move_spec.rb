aea6f324227f65b19e5119af203bbf4d9af81a26
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-09-30 12:41:40 -0600

    Being implementing ChessGame#make_move tests
    - Create several tests for the first moves of the game
      given a standard setup

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rspec'
 require_relative '../lib/chess_game'
+require_relative '../lib/invalid_move_error'
 
 # Require all the chess pieces
 Dir.children('./lib/chess_pieces').each { |piece_file| require_relative "../lib/chess_pieces/#{piece_file}" }
@@ -19,10 +20,40 @@ def setup_board(loc_to_piece_hash={})
   board
 end
 
-RSpec.describe 'ChessGame#move' do
+RSpec.describe 'ChessGame#make_move' do
   let(:game) { ChessGame.new }
   let(:board) { game.instance_variable_get(:@board) }
   
+
+  describe 'standard setup' do
+    context 'when moving pawns' do
+      it 'lets them move 1 space on their first turn' do
+        8.times do |col|
+          expect(proc { game.make_move([1, col], [2, col]) }).not_to raise_error
+          expect(proc { game.make_move([6, col], [5, col]) }).not_to raise_error
+        end
+      end
+
+      it 'lets them move 2 spaces on their first turn' do
+        8.times do |col|
+          expect(proc { game.make_move([1, col], [3, col]) }).not_to raise_error
+          expect(proc { game.make_move([6, col], [4, col]) }).not_to raise_error
+        end
+      end
+    end
+
+    context 'when moving knights' do
+      it 'lets the left knights move' do
+        expect(proc { game.make_move([0, 1], [2, 2]) }).not_to raise_error
+        expect(proc {game.make_move([7, 1], [5, 2])}).not_to raise_error
+      end
+
+      it 'lets the right knights move ' do
+        expect(proc { game.make_move([0, 6], [2, 5]) }).not_to raise_error
+        expect(proc {game.make_move([7, 6], [5, 5])}).not_to raise_error
+      end
+    end
+  end
 
 
 end
