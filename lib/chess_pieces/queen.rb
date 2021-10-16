6d7d99752e908eba4956c01af1eb24705344fcaf
Lukas <Lukas.Erekson@gmail.com>
2021-10-15 19:40:46 -0600

    Standardize Queen documentation

--- lib/chess_pieces/queen.rb
+++ lib/chess_pieces/queen.rb
@@ -8,9 +8,9 @@ class Queen < ChessPiece
   ##
   # Initializes a new queen piece with color and position.
   #
-  # +color+::     A string denoting the color of the piece.
-  # +position+::  An integer array of length 2 denoting the location of the
-  #               piece on the board.
+  # @param [String]         color     A string denoting the color of the piece.
+  # @param [Array<Integer>] position  An integer array of length 2 denoting the
+  #                                   location of the piece on the board.
   def initialize(color, position)
     @move_tree_template = build_queen_move_tree
     super(color == 'white' ? '♛'.white : '♕', color, position, 9)
@@ -21,6 +21,8 @@ class Queen < ChessPiece
   ##
   # Builds the Queen's move tree. The Queen can move in any direction as far as
   # the board permits.
+  #
+  # @return [MoveTree] move_tree_template A move tree template for the Queen.
   def build_queen_move_tree
     move_tree_template = MoveTree.new([0, 0])
     # Get directions
