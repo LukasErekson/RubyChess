d6d46645a1511ced066dae1af93694d9c92df046
Lukas <Lukas.Erekson@gmail.com>
2021-10-15 18:57:26 -0600

    Standardize bishop documentation

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -8,9 +8,9 @@ class Bishop < ChessPiece
   ##
   # Initializes a new bishop piece with color and position.
   #
-  # +color+::     A string denoting the color of the piece.
-  # +position+::  An integer array of length 2 denoting the location of the
-  #               piece on the board.
+  # @param [String]         color     A string denoting the color of the piece.
+  # @param [Array<Integer>] position  An integer array of length 2 denoting the
+  #                                   location of the piece on the board.
   def initialize(color, position)
     @move_tree_template = build_bishop_move_tree
     super(color == 'white' ? '♝'.white : '♗', color, position, 3)
@@ -21,6 +21,8 @@ class Bishop < ChessPiece
   ##
   # Builds a Bishop's move tree. The Bishop can move diagonally as far as the
   # board permits.
+  #
+  # @return [MoveTree] move_tree_template A move tree template for the bishop.
   def build_bishop_move_tree
     move_tree_template = MoveTree.new([0, 0])
 
