9ed54c1325e38710c2b4e3471449c91b08efa453
Lukas Erekson <Lukas.Erekson@gmail.com>
2021-10-28 12:14:50 -0600

    Create tests for ChessGame#check_game_over

--- spec/chess_game_spec.rb
+++ spec/chess_game_spec.rb
@@ -7,6 +7,19 @@ require_relative '../lib/invalid_move_error'
 # Require all the chess pieces
 Dir.children('./lib/chess_pieces').each { |piece_file| require_relative "../lib/chess_pieces/#{piece_file}" }
 
+##
+# Creates a board placing pieces using a location => piece hash.
+def setup_board(loc_to_piece_hash = {})
+  board = Array.new(8) { Array.new(8, ChessGame::BLANK_SQUARE) }
+
+  loc_to_piece_hash.each do |loc, piece|
+    row, col = loc
+    board[row][col] = piece
+  end
+
+  board
+end
+
 RSpec.describe ChessGame do
   let(:game) { ChessGame.new }
   let(:board) { game.instance_variable_get(:@board) }
@@ -79,6 +92,89 @@ RSpec.describe ChessGame do
         it "returns \"move\" when given \"#{str_input}\"" do
           expect(game.player_input_type(str_input)).to eq('move')
         end
+      end
+    end
+  end
+
+  describe '#check_game_over' do
+    context 'when white is in stalemate' do
+      it 'returns "Stalemate"' do
+        white_king = King.new('white', [0, 0])
+        pawn = Pawn.new('black', [2, 1])
+        black_king = King.new('black', [1, 2])
+        current_board = [pawn, black_king, white_king].each_with_object({}) do |piece, hash|
+          hash[piece.position] = piece
+        end
+        game.instance_variable_set(:@board, setup_board(current_board))
+        game.instance_variable_set(:@king_locs, { black: [1, 2], white: [0, 0] })
+        expect(game.check_game_over).to eq('Stalemate')
+      end
+
+      it 'returns "Stalemate"' do
+        white_king = King.new('white', [1, 0])
+        pawn = Pawn.new('white', [1, 1])
+        rook_1 = Rook.new('black', [0, 7])
+        rook_2 = Rook.new('black', [2, 7])
+        rook_3 = Rook.new('black', [2, 1])
+        current_board = [pawn, white_king, rook_1, rook_2, rook_3].each_with_object({}) do |piece, hash|
+          hash[piece.position] = piece
+        end
+        game.instance_variable_set(:@board, setup_board(current_board))
+        game.instance_variable_set(:@king_locs, { white: [1, 0] })
+        expect(game.check_game_over).to eq('Stalemate')
+      end
+    end
+
+    context 'when black is in stalemate' do
+      it 'returns "Stalemate"' do
+        black_king = King.new('black', [7, 7])
+        pawn = Pawn.new('white', [5, 6])
+        white_king = King.new('white', [6, 5])
+        current_board = [pawn, black_king, white_king].each_with_object({}) do |piece, hash|
+          hash[piece.position] = piece
+        end
+        game.instance_variable_set(:@current_player_color, 'black')
+        game.instance_variable_set(:@board, setup_board(current_board))
+        game.instance_variable_set(:@king_locs, { white: [6, 5], black: [7, 7] })
+        expect(game.check_game_over).to eq('Stalemate')
+      end
+
+      it 'returns "Stalemate"' do
+        black_king = King.new('black', [6, 7])
+        pawn = Pawn.new('black', [6, 6])
+        rook_1 = Rook.new('white', [7, 0])
+        rook_2 = Rook.new('white', [5, 0])
+        rook_3 = Rook.new('white', [5, 6])
+        current_board = [pawn, black_king, rook_1, rook_2, rook_3].each_with_object({}) do |piece, hash|
+          hash[piece.position] = piece
+        end
+        game.instance_variable_set(:@current_player_color, 'black')
+        game.instance_variable_set(:@board, setup_board(current_board))
+        game.instance_variable_set(:@king_locs, { black: [6, 7] })
+        expect(game.check_game_over).to eq('Stalemate')
+      end
+    end
+
+    context 'in checkmate situations' do
+      it 'returns "Checkmate"' do
+        black_king = King.new('black', [6, 7])
+        rook_1 = Rook.new('white', [7, 0])
+        rook_2 = Rook.new('white', [5, 0])
+        rook_3 = Rook.new('white', [6, 0])
+        current_board = [black_king, rook_1, rook_2, rook_3].each_with_object({}) do |piece, hash|
+          hash[piece.position] = piece
+        end
+        game.instance_variable_set(:@current_player_color, 'black')
+        game.instance_variable_set(:@check_in_play, true)
+        game.instance_variable_set(:@board, setup_board(current_board))
+        game.instance_variable_set(:@king_locs, { black: [6, 7] })
+        expect(game.check_game_over).to eq('Checkmate')
+      end
+    end
+
+    context 'in neither checkmate nor stalemate situations' do
+      it 'returns "continue"' do
+        expect(game.check_game_over).to eq('continue')
       end
     end
   end
